33c051db182a0c2ae9bb608a2c6c56f9ff8bd544
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-05-05 15:06:56 -0400

    add map method to show food categories

--- src/Components/Filter/FoodCategories.js
+++ src/Components/Filter/FoodCategories.js
@@ -1,5 +1,14 @@
+import Filter from "./Filter";
+
 const FoodCategories = () => {
-  return <div>FoodCategories</div>;
+  return (
+    <div>
+      <h1>What kind of food do you like?</h1>
+      {["ITALIAN", "SEAFOOD", "APPETIZERS", "SALADS", "ALL"].map((category) => (
+        <Filter category={category} />
+      ))}
+    </div>
+  );
 };
 
 export default FoodCategories;
