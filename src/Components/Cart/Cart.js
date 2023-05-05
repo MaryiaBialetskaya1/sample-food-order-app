e3b062f96ee4a71e19b61396b4603d5b4b4b3304
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-05-05 15:12:33 -0400

    add react-redux, redux/toolkit to project

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,5 +1,13 @@
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <img
+        className="cartIcon"
+        src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"
+        alt="cart"
+      />
+    </div>
+  );
 };
 
 export default Cart;
